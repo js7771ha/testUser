5a9570691c06d32fd19ad5c49f5c52205536c817
js7771ha <js7771ha@naver.com>
2019-11-02 18:43:11 +0900

    첫 commit

---
+++ config/test_user/test_con.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    "message" => [
+        "success" => "등록 완료",
+        "modify" => "수정 완료",
+        "delete" => "탈퇴 완료",
+        "delete_fail" => "탈퇴 실패",
+        "delete_input" => "탈퇴를 처리할 정보가 없습니다.",
+        "restore" => "복구 완료",
+        "restore_input" => "복구를 처리할 정보가 없습니다.",
+        "restore_fail" => "복구 실패",
+        "error" => "오류가 발생하였습니다.",
+        "not_match_pwd" => "비밀번호가 일치하지 않습니다.",
+        "input_id" => "아이디를 입력해주세요.",
+        "input_pwd" => "비밀번호를 입력하세요.",
+        "del_user" => "해당 유저를 삭제합니다.",
+        "ok_id" => "사용가능한 아이디 입니다.",
+        "no_id" => "사용할 수 없는 아이디 입니다.",
+    ],
+    "state" => 1
+];
